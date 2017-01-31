4509d26012f3b38bd52715b10ea5346f42cda6a9
Rainer Sigwald <raines@microsoft.com>
2017-01-31 10:00:02 -0600

    POC: Dumps information in inscrutable format

--- ParallelBuildDebuggingLogger/Class1.cs
+++ ParallelBuildDebuggingLogger/Class1.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace ParallelBuildDebuggingLogger
 {
-    public class Class1
+    public class Class1 : Logger
     {
+        private StringBuilder log;
+
+        private Dictionary<int, ProjectBuildInfo> buildInfos = new Dictionary<int, ProjectBuildInfo>();
+
+        public override void Initialize(IEventSource eventSource)
+        {
+            log = new StringBuilder();
+
+            eventSource.ProjectStarted += ProjectStartedHandler;
+        }
+
+        private void ProjectStartedHandler(object sender, ProjectStartedEventArgs projectStartedEventArgs)
+        {
+            var info = new ProjectBuildInfo(projectStartedEventArgs, buildInfos);
+
+            buildInfos.Add(info.ProjectInstanceId, info);
+
+            Console.WriteLine($"Project {info.ProjectInstanceId} built by project {info.ParentProjectInstanceId} with property diff {string.Join("; ", info.UniqueProperties.Select(up => $"{up.Key} = {up.Value}"))}");
+        }
     }
 }
---
+++ ParallelBuildDebuggingLogger/ProjectBuildInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace ParallelBuildDebuggingLogger
+{
+    class ProjectBuildInfo
+    {
+        public int ProjectInstanceId { get; set; }
+
+        public int ParentProjectInstanceId { get; set; }
+
+        public IDictionary<string, string> GlobalProperties { get; set; }
+        public Dictionary<string, string> UniqueProperties { get; set; } = new Dictionary<string, string>();
+
+        public ProjectBuildInfo(ProjectStartedEventArgs projectStartedEventArgs, IReadOnlyDictionary<int, ProjectBuildInfo> otherProjects)
+        {
+            ParentProjectInstanceId = projectStartedEventArgs.ParentProjectBuildEventContext.ProjectInstanceId;
+            ProjectInstanceId = projectStartedEventArgs.BuildEventContext.ProjectInstanceId;
+            GlobalProperties = projectStartedEventArgs.GlobalProperties;
+
+            foreach (var propertyName in GlobalProperties.Keys)
+            {
+                string parentValue;
+                if (
+                    otherProjects[ParentProjectInstanceId].GlobalProperties.TryGetValue(propertyName, out parentValue) &&
+                    parentValue == GlobalProperties[propertyName])
+                {
+                    // inherited from parent; uninteresting
+                }
+                else
+                {
+                    UniqueProperties[propertyName] = GlobalProperties[propertyName];
+                }
+            }
+        }
+    }
+}
