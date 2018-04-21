e09267bee9630cae058ed25dbb4ae4516f8c4ae6
Rainer Sigwald <raines@microsoft.com>
2018-04-20 22:48:08 -0500

    Report instances for each project
    Prints a report of each different project's different global properties.
    This doesn't help figure out _why_ there are different instances . . .
    yet.

--- ParallelBuildDebuggingLogger/Class1.cs
+++ ParallelBuildDebuggingLogger/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,7 @@ namespace ParallelBuildDebuggingLogger
 
             eventSource.ProjectStarted += ProjectStartedHandler;
             eventSource.TargetStarted += TargetStartedHandler;
+            eventSource.BuildFinished += BuildFinishedHandler;
         }
 
         private void TargetStartedHandler(object sender, TargetStartedEventArgs e)
@@ -35,14 +37,62 @@ namespace ParallelBuildDebuggingLogger
 
             if (buildInfos.ContainsKey(info.ProjectInstanceId))
             {
-                Console.WriteLine($"Reentering project {info} to build targets {info.StartedEventArgs.TargetNames}");
+                //Console.WriteLine($"Reentering project {info} to build targets {info.StartedEventArgs.TargetNames}");
             }
             else
             {
                 buildInfos.Add(info.ProjectInstanceId, info);
-                Console.WriteLine($"Project {info} built by project {info.ParentProjectInstanceId} -- targets '{info.StartedEventArgs.TargetNames}'");
+                //Console.WriteLine($"Project {info} built by project {info.ParentProjectInstanceId} -- targets '{info.StartedEventArgs.TargetNames}'");
+            }
+        }
+
+        private void BuildFinishedHandler(object sender, BuildFinishedEventArgs e)
+        {
+            var projects = new ConcurrentDictionary<string, List<ProjectBuildInfo>>();
+
+            foreach (var buildInfo in buildInfos.Values)
+            {
+                projects.GetOrAdd(
+                    buildInfo.StartedEventArgs.ProjectFile, 
+                    new List<ProjectBuildInfo>())
+                    .Add(buildInfo);
             }
 
+            foreach (var project in projects)
+            {
+                var path = project.Key;
+                var instances = project.Value;
+                if (instances.Count > 1)
+                {
+                    Console.WriteLine($"Project {path} was built multiple times:");
+
+                    var properties = new ConcurrentDictionary<string, string[]>();
+                    var differentProperties = new ConcurrentDictionary<string, string[]>();
+
+                    for (int i = 0; i < instances.Count; i++)
+                    {
+                        var instance = instances[i];
+
+                        foreach (var property in instance.GlobalProperties)
+                        {
+                            properties.GetOrAdd(property.Key, new string[instances.Count])[i] = property.Value;
+                        }
+                    }
+
+                    foreach (var item in properties)
+                    {
+                        if (!item.Value.All(p => p == item.Value[0]))
+                        {
+                            differentProperties[item.Key] = item.Value;
+                        }
+                    }
+
+                    for (int i = 0; i < instances.Count; i++)
+                    {
+                        Console.WriteLine(string.Join(" ", differentProperties.Select(kvp => kvp.Key + ": " + kvp.Value[i])));
+                    }
+                }
+            }
         }
     }
 }
